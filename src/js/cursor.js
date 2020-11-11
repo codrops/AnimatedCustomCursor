cb2f0eb7a389dc7057fe51d35c8ec4f8b9f5ebe2
ajmnz <arnaujim@gmail.com>
2020-11-11 23:38:06 +0100

    Hide cursor on window leave

--- src/js/cursor.js
+++ src/js/cursor.js
@@ -50,6 +50,14 @@ export default class Cursor extends EventEmitter {
             window.removeEventListener('mousemove', this.onMouseMoveEv);
         };
         window.addEventListener('mousemove', this.onMouseMoveEv);
+
+        this.isVisible = true;
+
+        this.hide = this.hide.bind(this);
+        this.show = this.show.bind(this);
+
+        document.body.addEventListener('mouseleave', this.hide);
+        document.body.addEventListener('mouseenter', this.show);
     }
     render() {
         this.renderedStyles['tx'].current = mouse.x - this.bounds.width/2;
@@ -62,7 +70,7 @@ export default class Cursor extends EventEmitter {
         this.DOM.el.style.transform = `translateX(${(this.renderedStyles['tx'].previous)}px) translateY(${this.renderedStyles['ty'].previous}px)`;
         this.DOM.circleInner.setAttribute('r', this.renderedStyles['radius'].previous);
 
-        requestAnimationFrame(() => this.render());
+        if (this.isVisible) requestAnimationFrame(() => this.render());
     }
     createTimeline() {
         // init timeline
@@ -91,6 +99,17 @@ export default class Cursor extends EventEmitter {
     leave() {
         this.renderedStyles['radius'].current = 60;
         this.tl.progress(1).kill();
+    }
+    show() {
+        this.isVisible = true;
+        requestAnimationFrame(() => this.render());
+        gsap.to(this.DOM.el, {duration: 0.9, ease: 'Power3.easeOut', opacity: 1});
+    }
+    hide(ev) {
+        if (!ev.relatedTarget && !ev.toElement) {
+            this.isVisible = false;
+            gsap.to(this.DOM.el, {duration: 0.9, ease: 'Power3.easeOut', opacity: 0});
+        }
     }
     listen() {
         this.on('enter', () => this.enter());
